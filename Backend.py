5458dec6bf8ea43bcd4c6bf7be4f19c161c7d182
Jones <jonesb@phys-121739.attlocal.net>
2018-11-18 16:23:56 -0600

    Update

--- Backend.py
+++ Backend.py
@@ -58,8 +58,8 @@ def SimpleModelDT(inp,t):
     outp[1] = (Gams[1] - k_ArAr1_Xe_ArXe*XeConc)*inp[1]
     outp[2] = (Gams[2] -(k_ArXe_Xe_XeXe1+k_ArXe_Xe_XeXe3)*XeConc)*inp[2]+k_ArAr3_Xe_ArXe*XeConc*inp[0]+k_ArAr1_Xe_ArXe*XeConc*inp[1]
     outp[3] = 0
-    outp[4] = Gams[4]*inp[4]+k_ArXe_Xe_XeXe1*XeConc*inp[2]
-    outp[5] = Gams[5]*inp[5]+k_ArXe_Xe_XeXe3*XeConc*inp[2]
+    outp[4] = Gams[4]*inp[4]+k_ArXe_Xe_XeXe3*XeConc*inp[2]
+    outp[5] = Gams[5]*inp[5]+k_ArXe_Xe_XeXe1*XeConc*inp[2]
     outp[6] = - (Gams[0]*inp[0]+Gams[1]*inp[1])
     outp[7] = - (Gams[2]*inp[2]+Gams[3]*inp[3])
     outp[8] = - (Gams[4]*inp[4]+Gams[5]*inp[5])
@@ -93,24 +93,24 @@ def SolveAndPlot(Log_XeConc,k_ArAr3_Xe_ArXe=100,k_ArAr1_Xe_ArXe = 100,k_ArXe_Xe_
 
     axs1.legend(loc='upper right')
     axs1.set_xlabel("Time / ns")
+    dt=t[1]-t[0]
+    Emis128=(y[1:,6]-y[:-1,6])/dt
+    Emis145=(y[1:,7]-y[:-1,7])/dt
+    Emis175=(y[1:,8]-y[:-1,8])/dt
 
-    Emis128=(y[1:,6]-y[:-1,6])/(t[1]-t[0])
-    Emis145=(y[1:,7]-y[:-1,7])/(t[1]-t[0])
-    Emis175=(y[1:,8]-y[:-1,8])/(t[1]-t[0])
-
-    NormMax=np.max(Emis128+Emis145+Emis175)
-    Emis128/=NormMax
-    Emis145/=NormMax
-    Emis175/=NormMax    
-    
+  #  NormMax=np.max(Emis128+Emis145+Emis175)
+  #  Emis128/=NormMax
+  #  Emis145/=NormMax
+  #  Emis175/=NormMax    
+    dt=t[-1]-t[0]
     axs2.plot(t[0:-1],Emis128,color='DarkBlue',label='128 nm')
     axs2.plot(t[0:-1],Emis145,color='purple',label='145 nm')
     axs2.plot(t[0:-1],Emis175,color='DarkRed',label='175 nm')
     axs2.legend(loc='center right')
     axs2.set_xlabel("Time / ns")
-    axs2.set_ylim(0,0.1)
+    axs2.set_ylim(0,0.01)
     axs2.set_title("Light Yields")
-    axs3.bar(['128','145','175','Total'],[sum(Emis128),sum(Emis145),sum(Emis175),sum(Emis128+Emis175+Emis145)],color=['DarkBlue','purple','DarkRed','black'])
+    axs3.bar(['128','145','175','Total'],[sum(Emis128)*dt,sum(Emis145)*dt,sum(Emis175)*dt,sum(Emis128+Emis175+Emis145)*dt],color=['DarkBlue','purple','DarkRed','black'])
     axs3.set_title("Total Light")
     axs3.set_xlabel("Wavelength")
 
